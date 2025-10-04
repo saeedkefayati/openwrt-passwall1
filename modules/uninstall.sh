3f97601c9a7f5c96a7c88e88e4d2bc9018ca8580
saeedkefayati <s.kefayati2012@gmail.com>
2025-10-04 14:37:07 +0330

    fix: update uninstall function

--- modules/uninstall.sh
+++ modules/uninstall.sh
@@ -3,9 +3,9 @@
 uninstall_passwall() {
     echo "Uninstalling Passwall v1..."
     /etc/init.d/passwall stop 
-    opkg remove luci-app-passwall
-    opkg autoremove
-    rm -rf /etc/config/passwall
-    rm -rf /usr/bin/passwall
+    opkg remove luci-app-passwall --autoremove || true
+    [ -d /etc/config/passwall ] && rm -rf /etc/config/passwall
+    [ -f /usr/bin/passwall1 ] && rm -f /usr/bin/passwall1
+    [ -f /root/passwall1 ] && rm -f /root/passwall1
     echo "Uninstallation completed successfully."
 }
